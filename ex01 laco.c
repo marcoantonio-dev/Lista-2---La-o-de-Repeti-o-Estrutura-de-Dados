064a6b5d422591b3bf69162e7506b13307fd0445
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-03-11 17:35:21 -0300

    exercicio 1, soma dos numeros de 1 a 100

---
+++ ex01 laco.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int i;
+    int soma = 0;
+
+        printf("Soma dos Numeros de 1 a 100 \n");
+    for (i = 1; i < 101; i++)
+    {
+        printf("%hd \n", soma = soma + i);
+    }
+
+}
