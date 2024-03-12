af28ba34cf08cb34f72c7ece3b7f22d1e3de7156
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-03-12 18:53:35 -0300

    ex05 nao acabado

---
+++ ex05.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+5. (WHILE) Faça um algoritmo que determine e escreva:   
+
+- a maior idade dos habitantes
+
+- a quantidade de indivíduos do sexo feminino cuja idade está entre 18 e 35 anos inclusive e que tenham olhos verdes e cabelos
+ louros.
+
+O final do conjunto de habitantes é reconhecido pelo valor -1 entrada como idade.
+*/
+
+int main()
+{
+
+    int idade, maior_idade, qntdMulheres, i, j = 0;
+    char sexo;
+    char corOlhos, corCabelos;
+
+    while (i >= 0)
+    {
+        printf("Digite sua idade: ");
+        scanf("%d", &idade);
+
+        if (maior_idade < idade)
+        {
+            maior_idade = idade;
+        }else{
+            maior_idade = maior_idade;
+        }
+
+        
+        
+
+
+    }
+    
+
+}
